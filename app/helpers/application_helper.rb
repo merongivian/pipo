170035a0f63b031450d46d6e9683a1b32bf3ada5
Jose Anasco <joseanasco1@gmail.com>
2021-01-01 17:44:03 +0100

    assets not found in production

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def embedded_svg(filename, options = {})
-    assets = Rails.application.assets
+    asset = Rails.application.assets || Rails.application.config.assets
     file = assets.find_asset(filename).source.force_encoding("UTF-8")
     doc = Nokogiri::HTML::DocumentFragment.parse file
     svg = doc.at_css "svg"
