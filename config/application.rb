c61bf6b8addb9dab2911ea70ed461f388e546dc8
Jose Anasco <joseanasco1@gmail.com>
2021-01-01 17:10:07 +0100

    include icons path for compilation

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module Pipo
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.assets.paths << Rails.root.join("app","assets", "icons")
   end
 end
